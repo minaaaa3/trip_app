dd6e4fac74df08fc040ad99cf74da4d0d471bc23
minaaaa3 <qianqingjingshang312@gmail.com>
2025-10-17 02:12:45 +0900

    prisma型エラー解消

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,7 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     ignores: [
+      "lib/**/*",
       "node_modules/**",
       ".next/**",
       "out/**",
