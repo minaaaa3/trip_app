d0d9cfb2d9933099af14c02b8f043651e269b3fc
minaaaa3 <qianqingjingshang312@gmail.com>
2025-10-22 01:31:14 +0900

    prismaのパス修正

--- app/api/trips/[tripId]/spots/[spotId]/route.ts
+++ app/api/trips/[tripId]/spots/[spotId]/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 
-import { Prisma } from "@prisma/client";
+import { Prisma } from "@/lib";
 
 /**
  * PUT /api/trips/{tripId}/spots/{spotId}
